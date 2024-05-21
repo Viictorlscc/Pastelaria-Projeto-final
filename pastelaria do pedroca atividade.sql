06568fd997f34c7c0a39df49ff388836a5af7f27
Luiz Eduardo <146491566+dudunasc@users.noreply.github.com>
2024-05-21 20:13:13 -0300

    Criação da Pastelaria do Pedroca
    Aqui foi criado a DATABASE da pastelaria e a tabela clientes, onde estarão armazenados os dados pessoais do cliente

---
+++ pastelaria do pedroca atividade.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE pastelaria_do_pedroca;
+USE pastelaria_do_pedroca;
+
+CREATE TABLE clientes_pastelaria(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    apelido VARCHAR(15),
+    data_nascimento CHAR(8),
+    telefone CHAR(15) NOT NULL,
+    cpf CHAR(14) NOT NULL UNIQUE,
+    email VARCHAR(40) UNIQUE,
+    cep CHAR(8),
+    rua VARCHAR(50) NOT NULL,
+    casa VARCHAR(30), 
+    bairro VARCHAR(30),
+    cidade (30)
+);
+
