3140679932d8138c458632b603c05691a7929fb4
JOÃO VICTOR GONÇALVES <dgvictorsilva@gmail.com>
2024-05-27 21:49:12 -0300

    ATUALIZAÇÃO DO CODIGO SQL E DIAGRAMA LOGICO

--- pastelaria do pedroca atividade.sql
+++ pastelaria do pedroca atividade.sql
@@ -1,18 +1,66 @@
-CREATE DATABASE pastelaria_do_pedroca;
-USE pastelaria_do_pedroca;
+CREATE DATABASE pastelaria_do_Vidu;
+USE pastelaria_do_Vidu;
 
 CREATE TABLE clientes_pastelaria(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    apelido VARCHAR(15),
-    data_nascimento CHAR(8),
-    telefone CHAR(15) NOT NULL UNIQUE,
-    cpf CHAR(14) NOT NULL UNIQUE,
-    email VARCHAR(40) UNIQUE,
-    cep CHAR(8),
-    rua VARCHAR(50) NOT NULL,
-    casa VARCHAR(30), 
-    bairro VARCHAR(30),
-    cidade (30)
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL,
+apelido VARCHAR(15),
+data_nascimento CHAR(8),
+telefone CHAR(15) NOT NULL UNIQUE,
+cpf CHAR(14) NOT NULL UNIQUE,
+email VARCHAR(40) UNIQUE,
+cep CHAR(8),
+rua VARCHAR(50) NOT NULL,
+casa INT, 
+bairro VARCHAR(30),
+cidade VARCHAR(45)
 );
 
+CREATE TABLE pastel(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50),
+categoria VARCHAR(45),
+tamanho VARCHAR(15)
+);
+
+CREATE TABLE recheios (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+ingredientes TEXT,
+preco DECIMAL(10,2)
+);
+
+CREATE TABLE pastel_recheio (
+id_pastel INT,
+id_recheio INT,
+FOREIGN KEY (id_pastel) REFERENCES pastel(id),
+FOREIGN KEY (id_recheio) REFERENCES recheios(id),
+PRIMARY KEY (id_pastel, id_recheio)
+);
+
+CREATE TABLE Pedidos (
+id INT PRIMARY KEY AUTO_INCREMENT,
+id_cliente INT,
+data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+forma_pagamento VARCHAR(50),
+FOREIGN KEY (id_cliente) REFERENCES clientes_pastelaria(id)
+);
+
+CREATE TABLE Precos_Pasteis (
+id INT PRIMARY KEY AUTO_INCREMENT,
+id_pastel INT,
+id_recheio INT,
+tamanho VARCHAR(20),
+preco DECIMAL(10,2),
+FOREIGN KEY (id_pastel) REFERENCES pastel(id),
+FOREIGN KEY (id_recheio) REFERENCES recheios(id)
+);
+
+CREATE TABLE Pedido_Pastel (
+id_pedido INT,
+id_pastel INT,
+quantidade INT,
+FOREIGN KEY (id_pedido) REFERENCES Pedidos(id),
+FOREIGN KEY (id_pastel) REFERENCES pastel(id),
+PRIMARY KEY (id_pedido, id_pastel)
+);
